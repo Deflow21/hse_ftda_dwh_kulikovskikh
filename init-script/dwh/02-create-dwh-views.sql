9ab55c90c1da6bfad8770509c42a2272d98f0a5d
Denis <64776483+Deflow21@users.noreply.github.com>
2024-12-06 15:00:53 +0300

    Update 02-create-dwh-views.sql

--- init-script/dwh/02-create-dwh-views.sql
+++ init-script/dwh/02-create-dwh-views.sql
@@ -1,26 +1,45 @@
--- Создание представления для анализа пассажиропотока
 CREATE OR REPLACE VIEW dwh_detailed.passenger_flow_view AS
+WITH departure_flights AS (
+    SELECT
+        ha.airport_code,
+        hf.flight_id
+    FROM dwh_detailed.hub_airports ha
+    JOIN dwh_detailed.link_flights_departure_airports lfda ON lfda.airport_id = ha.airport_id
+    JOIN dwh_detailed.hub_flights hf ON hf.flight_id = lfda.flight_id
+),
+arrival_flights AS (
+    SELECT
+        ha.airport_code,
+        hf.flight_id
+    FROM dwh_detailed.hub_airports ha
+    JOIN dwh_detailed.link_flights_arrival_airports lfaa ON lfaa.airport_id = ha.airport_id
+    JOIN dwh_detailed.hub_flights hf ON hf.flight_id = lfaa.flight_id
+),
+departure_tickets AS (
+    SELECT
+        df.airport_code,
+        ht.ticket_id
+    FROM departure_flights df
+    JOIN dwh_detailed.link_tickets_flights ltf ON ltf.flight_id = df.flight_id
+    JOIN dwh_detailed.hub_tickets ht ON ht.ticket_id = ltf.ticket_id
+),
+arrival_tickets AS (
+    SELECT
+        af.airport_code,
+        ht.ticket_id
+    FROM arrival_flights af
+    JOIN dwh_detailed.link_tickets_flights ltf ON ltf.flight_id = af.flight_id
+    JOIN dwh_detailed.hub_tickets ht ON ht.ticket_id = ltf.ticket_id
+)
 SELECT
-    h_airport.airport_code,
-    COUNT(DISTINCT h_flight.flight_id) AS departure_flights_num,
-    COUNT(DISTINCT h_ticket.ticket_id) AS departure_psngr_num,
-    COUNT(DISTINCT h_flight_arr.flight_id) AS arrival_flights_num,
-    COUNT(DISTINCT h_ticket_arr.ticket_id) AS arrival_psngr_num
-FROM dwh_detailed.hub_airports h_airport
-LEFT JOIN dwh_detailed.sat_flight_details s_flight
-    ON h_airport.airport_code = s_flight.departure_airport
-LEFT JOIN dwh_detailed.hub_flights h_flight
-    ON s_flight.flight_id = h_flight.flight_id
-LEFT JOIN dwh_detailed.sat_ticket_details s_ticket
-    ON h_flight.flight_id = CAST(s_ticket.book_ref AS INTEGER)
-LEFT JOIN dwh_detailed.hub_tickets h_ticket
-    ON s_ticket.ticket_id = h_ticket.ticket_id
-LEFT JOIN dwh_detailed.sat_flight_details s_flight_arr
-    ON h_airport.airport_code = s_flight_arr.arrival_airport
-LEFT JOIN dwh_detailed.hub_flights h_flight_arr
-    ON s_flight_arr.flight_id = h_flight_arr.flight_id
-LEFT JOIN dwh_detailed.sat_ticket_details s_ticket_arr
-    ON h_flight_arr.flight_id = CAST(s_ticket_arr.book_ref AS INTEGER)
-LEFT JOIN dwh_detailed.hub_tickets h_ticket_arr
-    ON s_ticket_arr.ticket_id = h_ticket_arr.ticket_id
-GROUP BY h_airport.airport_code;
+    ha.airport_code,
+    COUNT(DISTINCT df.flight_id) as departure_flights_num,
+    COUNT(DISTINCT dt.ticket_id) as departure_psngr_num,
+    COUNT(DISTINCT af.flight_id) as arrival_flights_num,
+    COUNT(DISTINCT at.ticket_id) as arrival_psngr_num
+FROM dwh_detailed.hub_airports ha
+LEFT JOIN departure_flights df ON df.airport_code = ha.airport_code
+LEFT JOIN arrival_flights af ON af.airport_code = ha.airport_code
+LEFT JOIN departure_tickets dt ON dt.airport_code = ha.airport_code
+LEFT JOIN arrival_tickets at ON at.airport_code = ha.airport_code
+GROUP BY ha.airport_code;
